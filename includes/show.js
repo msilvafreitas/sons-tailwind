b2e1a16d2f3c3ce13e3c1d41e508c6ddde90681d
matheus <freitas1matheus@gmail.com>
2022-09-07 23:21:16 -0300

    Update show.js

---
+++ includes/show.js
@@ -0,0 +1,8 @@
+function showPlayers() {
+    var players = document.getElementById("players");
+    if (players.style.display === "none") {
+      players.style.display = "block";
+    } else {
+      players.style.display = "none";
+    }
+  } 
